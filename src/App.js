21566be63d53abad7dc655a9706d682cd4bbe614
Sen <bsenthoor@gmail.com>
2022-11-16 20:12:42 +0530

    Filter functionality complete. Basic to medium styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CountryCard from "./CountryCard";
+
+const App = _ =>{
+
+  const [countries, setCountries] = useState([])
+  const [countryInput, setCountryInput] = useState('')
+  const [countriesToShow, setCountriesToShow] = useState([])
+
+  //========================  useEffect to fetch list of all countries  =========================
+  useEffect( () =>{
+    axios.get('https://restcountries.com/v3.1/all')
+      .then( response => {
+
+        //===================== Sorting the countries in alphabetical order ====================
+        response.data.sort( (a, b) => {
+          a = a.name['common'];
+          b = b.name['common'];
+          if (a<b){
+            return -1;
+          }
+          else if (a === b){
+            return 0;
+          }
+          else{
+            return 1;
+          }
+        })
+        //==================== end of sort ================================
+        setCountries(response.data);
+        setCountriesToShow(response.data);
+      })
+      .catch( err => console.log('Error connecting to countries backend:', err) )
+  },[])
+
+  //====================  Country Input Field change handler  ======================
+  const handleCountryInpChange = e =>{
+    let inp = e.target.value;
+    setCountryInput(inp);
+    let re = new RegExp(inp, 'gi');
+    let filteredCountries = countries.filter( country => country.name['common'].match(re) );
+    setCountriesToShow(filteredCountries);
+  }
+
+  console.log(countriesToShow);
+  return(
+    <>
+      <section id="Filter countries">
+        <input type="text" name="country" placeholder="Country name" onChange={handleCountryInpChange} value={countryInput}/>
+      </section>
+
+      <section id="Country-List">
+        <h2>Filtered Countries</h2>
+        <div className="country-card-container">
+          {countriesToShow.map( (country, index) => <a key={index}> <CountryCard country={country}/> </a>)}
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default App;
---
+++ src/CountryCard.js
@@ -0,0 +1,28 @@
+import './countryCard.css'
+
+const CountryCard = ({country}) =>{
+    return(
+        <div className="country-card">
+            <div className="img-container">
+                <img src={country.flags.png} alt={`${country.name.common}'s flag`} />
+            </div>
+
+            <div className="txt-container">
+                <h3>{country.name.common}</h3>
+
+                <div className="continent-info">
+                    <div className="continent">Continent: {country.continents.map( (continent, index) => <span key={index}>{`${continent} `}</span>)}</div>
+                    <div className="region">Region: {country.subregion}</div>
+                </div>
+
+                <div className="country-info">
+                    <div className="capital">Capital: {country.capital}</div>
+                    <div className="capital">Area: {country.area} km<sup>2</sup></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CountryCard;
